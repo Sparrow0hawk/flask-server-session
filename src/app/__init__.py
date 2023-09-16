398613449545d4baca1a64520d75ff50fcd8ba5e
Sparrow0hawk <alexjcoleman@gmail.com>
2023-09-16 11:55:56 +0100

    Add Flask-Session and endpoint for setting 'user' key

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for
+from flask_session import Session
 
 
 def create_app() -> Flask:
 
     app = Flask(__name__)
+    app.config.from_mapping(
+        SESSION_TYPE="filesystem",
+        SESSION_FILE_DIR="cache"
+    )
+    Session(app)
 
     @app.route("/")
     def index() -> str:
-        return "<h1>Hello world!</h1>"
+        if session.get("user"):
+            return f"<h1>Hello {session['user']}!</h1>"
+        else:
+            return "<h1>Hello world!</h1>"
+
+    @app.route("/user/")
+    def user():
+        session["user"] = "user1"
+        return redirect(url_for("index"))
 
     return app
