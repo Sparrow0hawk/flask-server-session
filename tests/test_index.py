038197ab5cd770346093098c1ff52da9a24392dc
Sparrow0hawk <alexjcoleman@gmail.com>
2023-09-16 11:56:33 +0100

    Add test for server side session

--- tests/test_index.py
+++ tests/test_index.py
@@ -3,3 +3,9 @@ import pytest
 def test_index(client):
     response = client.get("/")
     assert b"<h1>Hello world!</h1>" in response.data
+
+def test_session(client):
+    with client.session_transaction() as session:
+        session["user"] = "user1"
+    response = client.get("/")
+    assert b"<h1>Hello user1!</h1>" in response.data
